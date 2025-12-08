b8cd6b379d206e8a192f1c8f35fc7d6771ff57fa
DEGUENON <morgineuf@gmail.com>
2025-12-08 23:18:02 +0100

    Add Regionseeder with Atacora and Atlantique regions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this ->call([
          
             Langueseeder::class,
+            Regionseeder::class,
             utilisateurseeder::class,
         ]);
         
